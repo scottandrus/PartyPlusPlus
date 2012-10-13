f3217c06e66144507de082eddb2c3203bdc6e5be
gg67 <graham.gaylor@gmail.com>
2012-10-12 21:47:10 -0500

    Added PPPLoginViewController

---
+++ PartyPlusPlus/PPPLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  PPPLoginViewController.h
+//  PartyPlusPlus
+//
+//  Created by Graham Gaylor on 10/12/12.
+//  Copyright (c) 2012 Tapatory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPPLoginViewController : UIViewController
+
+@end
---
+++ PartyPlusPlus/PPPLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  PPPLoginViewController.m
+//  PartyPlusPlus
+//
+//  Created by Graham Gaylor on 10/12/12.
+//  Copyright (c) 2012 Tapatory. All rights reserved.
+//
+
+#import "PPPLoginViewController.h"
+
+@interface PPPLoginViewController ()
+
+@end
+
+@implementation PPPLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
